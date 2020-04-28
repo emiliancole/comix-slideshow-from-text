1c74106a159a5210e434de7229cd35ab163f87c5
Emilian Cole <emiliancole@gmail.com>
2020-04-28 12:14:59 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="generator" content="AlterVista - Editor HTML"/>
+<?php
+include("setting.php");
+?>
+  <title><?= $ctitle; ?> - Index</title>
+</head>
+<body style="background-color:orange" >
+  <p>&nbsp;<a href="index.php">COMIX</a>
+  - Read <?= $ctitle; ?> online.</p>
+<a href="<?= $dirname; ?>.01.php"><img src="<?= $i01[1]; ?>" alt="01" width="150" /></a>
+
+</body>
+</html>
