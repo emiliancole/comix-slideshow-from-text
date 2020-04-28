acccc3d3e18e07d2489c0652c8283e1ce8e737e1
Emilian Cole <emiliancole@gmail.com>
2020-04-28 12:17:49 +0200

    Create setting.php

---
+++ setting.php
@@ -0,0 +1,11 @@
+<?php
+$cname="SOLOMON KANE-01";
+$ctitle="SOLOMON KANE";
+$dirname="SolomonKane";
+$fname="SolomonKane.01.php";
+$lastpage=27;
+$pad=3;
+$i01=file("SolomonKane.01.img.txt");
+$images = file("SolomonKane.01.img.txt");
+
+?>
